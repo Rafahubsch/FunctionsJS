58ecdb4d058cf9a874eebb1ca6b136410e653ebe
Rafahubsch <rafaela.hubsch@edu.senai.br>
2026-08-13 10:46:13 -0300

    adicionar o ex9.js

---
+++ ex9.js
@@ -0,0 +1,8 @@
+function celsiusParaFahrenheit(celsius){
+    let fahrenheit= (celsius * 9 / 5) + 32
+
+    return fahrenheit;
+}
+let resultado = celsiusParaFahrenheit(32)
+
+console.log (resultado)
