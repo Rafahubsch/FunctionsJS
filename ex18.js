b0a059311c6d0406255e4b5d5b3f7903258a728f
Rafahubsch <rafaela.hubsch@edu.senai.br>
2026-08-13 11:46:24 -0300

    adicionar o ex18.js

---
+++ ex18.js
@@ -0,0 +1,5 @@
+function criarMensagem(nome, curso) {
+    return (`Olá, ${nome}! Você está matriculada no curso de ${curso}.`);
+}
+
+console.log(criarMensagem("Ana", "Desenvolvimento de Sistemas"));
